7c37c3b246c080635d090f5510fa82a7a0dbd110
홍혁준 <jjj8337721@naver.com>
2022-09-12 18:33:13 +0900

    feat : Payment Auditing & Entity 생성

--- MSA-OnlineWorkout/owo-payment/src/main/java/kr/co/owopayment/domain/entity/PaymentEntity.java
+++ MSA-OnlineWorkout/owo-payment/src/main/java/kr/co/owopayment/domain/entity/PaymentEntity.java
@@ -1,4 +1,19 @@
 package kr.co.owopayment.domain.entity;
 
-public class PaymentEntity {
+import kr.co.owopayment.domain.shared.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_payment")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "payment_id"))
+@Getter
+public class PaymentEntity extends BaseEntity {
 }
---
+++ MSA-OnlineWorkout/owo-payment/src/main/java/kr/co/owopayment/domain/shared/BaseEntity.java
@@ -0,0 +1,31 @@
+package kr.co.owopayment.domain.shared;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+// 객체 입장에서 공통 매핑 정보가 필요할 때 사용
+@MappedSuperclass
+// 공통적으로 가지고 있는 필드나 컬럼들을 시간에 대해서 자동으로 값을 넣어주는 기능
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(of="id", callSuper = false)
+@Getter
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @CreatedDate            //최초 생성 시간
+    @Column(name = "created_at", nullable = false, updatable = false)
+    protected LocalDateTime createdAt = null;
+
+    @LastModifiedDate       //마지막 수정 시간
+    @Column(name = "updated_at")
+    protected LocalDateTime updatedAt;
+}
