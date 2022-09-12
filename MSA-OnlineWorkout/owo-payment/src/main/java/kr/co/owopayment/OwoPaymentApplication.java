cf2827caac3f87440e0a57a6204249759c8a32ec
홍혁준 <jjj8337721@naver.com>
2022-09-12 17:41:05 +0900

    feat : payment eureka에 등록

--- MSA-OnlineWorkout/owo-payment/src/main/java/kr/co/owopayment/OwoPaymentApplication.java
+++ MSA-OnlineWorkout/owo-payment/src/main/java/kr/co/owopayment/OwoPaymentApplication.java
@@ -2,8 +2,10 @@ package kr.co.owopayment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OwoPaymentApplication {
 
     public static void main(String[] args) {
