aeb8cacf0ff139aff6d0bc27514ed30b8ed1ec21
홍혁준 <jjj8337721@naver.com>
2022-09-12 17:59:49 +0900

    feat : Member Entity 로직 구현

--- MSA-OnlineWorkout/owo-member/src/main/java/kr/co/owomember/domain/entity/MemberEntity.java
+++ MSA-OnlineWorkout/owo-member/src/main/java/kr/co/owomember/domain/entity/MemberEntity.java
@@ -1,4 +1,42 @@
 package kr.co.owomember.domain.entity;
 
-public class MemberEntity {
+import kr.co.owomember.domain.shared.BaseEntity;
+import kr.co.owomember.domain.shared.enums.MemberRole;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tbl_member")
+@Getter
+@AttributeOverride(name = "id", column = @Column(name = "member_id"))
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberEntity extends BaseEntity {
+
+    @Column(name = "identity", unique = true, nullable = false, length = 100)
+    private String identity;
+
+    @Column(name = "password", length = 100)
+    private String password;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @Column(name = "member_role")
+    @Enumerated(value = EnumType.STRING)
+    private MemberRole memberRole;
+
+    @Builder
+    public MemberEntity(String identity,
+                        String password,
+                        String name,
+                        MemberRole memberRole) {
+        this.identity = identity;
+        this.password = password;
+        this.name = name;
+        this.memberRole = memberRole;
+    }
 }
