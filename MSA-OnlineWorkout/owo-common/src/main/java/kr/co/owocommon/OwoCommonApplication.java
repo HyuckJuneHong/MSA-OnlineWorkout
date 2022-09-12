b670c37c375123632408b34a1336cdac98b2eb99
홍혁준 <jjj8337721@naver.com>
2022-09-12 16:23:04 +0900

    feat : init

---
+++ MSA-OnlineWorkout/owo-common/src/main/java/kr/co/owocommon/OwoCommonApplication.java
@@ -0,0 +1,13 @@
+package kr.co.owocommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwoCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwoCommonApplication.class, args);
+    }
+
+}
---
+++ MSA-OnlineWorkout/owo-common/src/test/java/kr/co/owocommon/OwoCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.owocommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwoCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
