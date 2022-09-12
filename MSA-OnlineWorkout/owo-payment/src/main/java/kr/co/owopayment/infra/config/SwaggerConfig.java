72d05deeed7cc12d231851173fb7188cf9e86a44
홍혁준 <jjj8337721@naver.com>
2022-09-12 18:33:31 +0900

    feat : Swagger & Web Config 생성

---
+++ MSA-OnlineWorkout/owo-payment/src/main/java/kr/co/owopayment/infra/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package kr.co.owopayment.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    //API 정보, 보안 컨텍스트 및 보안 체계를 포함하도록 API Docket 빈을 구성합니다.
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
---
+++ MSA-OnlineWorkout/owo-payment/src/main/java/kr/co/owopayment/infra/config/WebConfig.java
@@ -0,0 +1,21 @@
+package kr.co.owopayment.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final long MAX_AGE_SECOND = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECOND);
+    }
+}
