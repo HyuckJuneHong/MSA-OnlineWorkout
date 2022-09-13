2b3abdf765a844438d5b436af43fd1973edf23de
홍혁준 <jjj8337721@naver.com>
2022-09-13 10:40:30 +0900

    feat : payment API 설정

--- MSA-OnlineWorkout/owo-payment/src/main/java/kr/co/owopayment/controller/PaymentController.java
+++ MSA-OnlineWorkout/owo-payment/src/main/java/kr/co/owopayment/controller/PaymentController.java
@@ -1,17 +1,18 @@
 package kr.co.owopayment.controller;
 
 import io.swagger.annotations.ApiOperation;
+import kr.co.owopayment.domain.dto.PaymentDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/payments")
+@RequestMapping("/gateway/payments")
 @RequiredArgsConstructor
 public class PaymentController {
 
     @ApiOperation("상품 등록")
     @PostMapping("/create")
-    public void create(){
+    public void create(@RequestBody PaymentDto.CREATE_PAYMENT payment){
         //TODO: create Payment
     }
 
@@ -23,19 +24,19 @@ public class PaymentController {
 
     @ApiOperation("상품 정보 수정")
     @PutMapping("/update")
-    public void update(){
+    public void update(@RequestBody PaymentDto.UPDATE_PAYMENT payment){
         //TODO: update
     }
 
     @ApiOperation("상품 정보 조회")
     @GetMapping()
-    public void getPayment(){
+    public void getPayment(@RequestBody PaymentDto.GET_PAYMENT payment){
         //TODO: get Payment
     }
 
     @ApiOperation("상품 삭제")
     @DeleteMapping("/delete")
-    public void deleteMember(){
+    public void deleteMember(@RequestBody PaymentDto.DELETE_PAYMENT payment){
         //TODO: delete Payment
     }
 }
---
+++ MSA-OnlineWorkout/owo-payment/src/main/java/kr/co/owopayment/domain/dto/PaymentDto.java
@@ -0,0 +1,37 @@
+package kr.co.owopayment.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+public class PaymentDto {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class CREATE_PAYMENT{
+        //TODO payment create
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class UPDATE_PAYMENT{
+        //TODO payment update
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class GET_PAYMENT{
+        //TODO payment get
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class DELETE_PAYMENT{
+        //TODO payment delete
+    }
+
+}
