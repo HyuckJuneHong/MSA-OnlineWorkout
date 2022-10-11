9e35183378c9a489616fdfc4731fbe6af64cd353
홍혁준 <jjj8337721@naver.com>
2022-10-11 10:15:36 +0900

    feat : Gateway에 Config 설정 및 Actuator 설정

--- MSA-OnlineWorkout/owo-gateway/src/main/java/kr/co/owogateway/OwoGatewayApplication.java
+++ MSA-OnlineWorkout/owo-gateway/src/main/java/kr/co/owogateway/OwoGatewayApplication.java
@@ -1,11 +1,8 @@
 package kr.co.owogateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
-import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -13,10 +10,5 @@ public class OwoGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OwoGatewayApplication.class, args);
-    }
-
-    @Bean
-    public HttpTraceRepository httpTraceRepository(){
-        return new InMemoryHttpTraceRepository();
     }
 }
