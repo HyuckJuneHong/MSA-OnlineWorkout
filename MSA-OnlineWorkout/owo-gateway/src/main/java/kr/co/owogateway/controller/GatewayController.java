210aaec8be1cd159861285a4337afcd3d956d9a2
홍혁준 <jjj8337721@naver.com>
2022-10-11 13:40:22 +0900

    feat : application -dev & prod

--- MSA-OnlineWorkout/owo-gateway/src/main/java/kr/co/owogateway/controller/GatewayController.java
+++ MSA-OnlineWorkout/owo-gateway/src/main/java/kr/co/owogateway/controller/GatewayController.java
@@ -1,13 +1,28 @@
 package kr.co.owogateway.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class GatewayController {
+
+    private final Environment environment;
 
     @GetMapping
     public String test(){
         return "gateway-test";
     }
+
+    @GetMapping("/config")
+    public String configCheck(){
+        return String.format("It's Working in Gateway Service %s \n %s \n %s \n %s"
+                , environment.getProperty("local.server.port")
+                , environment.getProperty("server.port")
+                , environment.getProperty("jwt.secret")
+                , environment.getProperty("jwt.description"));
+    }
+
 }
